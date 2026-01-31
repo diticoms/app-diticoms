093c5f6b66fe0cfc5b744890c44cdd33d181c981
diticoms <diticoms@gmail.com>
2026-01-31 10:01:42 +0000

    Deploying to gh-pages from @ diticoms/app-diticoms@f86a23a7169b446f33a0bd58fce77fe52f74c767 🚀

--- constants.ts
+++ constants.ts
@@ -1,4 +1,4 @@
-import { AppConfig } from './types';
+import { AppConfig } from './types.ts';
 
 export const ACCESS_CODE = "123456"; 
 export const CURRENT_VERSION = "1.0.1";
@@ -6,7 +6,6 @@ export const GITHUB_REPO = "https://github.com/diticoms/diticoms-app";
 export const VERSION_CHECK_URL = "https://raw.githubusercontent.com/diticoms/diticoms-app/main/version.json";
 export const DOMAIN_URL = "https://service.diticoms.vn";
 
-// API URL MỚI ĐÃ ĐƯỢC CẬP NHẬT
 export const SHEET_API_URL = "https://script.google.com/macros/s/AKfycbwJZ1eTmohBOqDgUPugEJQKoF-1YtJ1PW-M7LQc1dzCKdoc8D04uNisfFoDAryUeF7K/exec";
 
 export const DEFAULT_CONFIG: AppConfig = {
