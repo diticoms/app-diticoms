d59c86e9fcc44fde79ee3e6dae2e79185f438cb3
diticoms <diticoms@gmail.com>
2026-02-01 14:00:55 +0700

    Auto-deploy: Cập nhật hệ thống

--- index.tsx
+++ index.tsx
@@ -2,6 +2,16 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 
+const removeSplash = () => {
+  const splash = document.getElementById('splash');
+  if (splash) {
+    splash.style.opacity = '0';
+    setTimeout(() => {
+      if (splash.parentNode) splash.remove();
+    }, 500);
+  }
+};
+
 const rootElement = document.getElementById('root');
 if (rootElement) {
   const root = createRoot(rootElement);
@@ -10,4 +20,7 @@ if (rootElement) {
       <App />
     </React.StrictMode>
   );
+  
+  // Gỡ màn hình chờ sau khi ứng dụng bắt đầu render
+  setTimeout(removeSplash, 1200);
 }
